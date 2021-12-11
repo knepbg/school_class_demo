e9f279f46bd7905ed671c18be5f8130b3603b13b
knepbg <knep.87@gmail.com>
2021-12-11 12:56:46 +0200

    Configuration  methods in SchoolerServiceImp and SchoolerRepository. Fix problems in  ClasssRoomRepository, ClassRoomSeviceImpl

--- src/main/java/com/schoolclass/demo/repository/ClassRoomRepository.java
+++ src/main/java/com/schoolclass/demo/repository/ClassRoomRepository.java
@@ -4,8 +4,10 @@ import com.schoolclass.demo.model.ClassRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ClassRoomRepository extends JpaRepository<ClassRoom, Long> {
 
-    ClassRoom findByName(String classRoomName);
+    Optional<ClassRoom> findByName(String classRoomName);
 }
--- src/main/java/com/schoolclass/demo/repository/SchoolerRepository.java
+++ src/main/java/com/schoolclass/demo/repository/SchoolerRepository.java
@@ -2,8 +2,19 @@ package com.schoolclass.demo.repository;
 
 import com.schoolclass.demo.model.Schooler;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface SchoolerRepository extends JpaRepository<Schooler, Long> {
+
+    Optional<Schooler> findByNumberInClass(@Param("numberInClass") String numberInClass);
+
+    Set<Schooler> findByFirstName(@Param("firstName") String firstName);
+
+    Set<Schooler> findByLastName(@Param("lastName") String lastName);
+
 }
